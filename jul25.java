5a0eb18646fbd0802223932958995b4b59e2d479
jakkulavenkatarahul11 <rahulyadavnani11@gmail.com>
2024-07-26 09:13:33 +0530

    july 25 file added

---
+++ jul25.java
@@ -0,0 +1,43 @@
+package Training;
+// armstronf program :-
+// public class jul25 {
+//     public static void main(String[] args) {
+//         int n = 1634, am = 0 , temp ;
+//         int org = n;
+//         while ( n > 0){
+//             temp = n % 10 ;
+//             temp = temp * temp * temp * temp;
+//             am = am + temp;
+//             n = n / 10;
+//         }
+//         if( org == am){
+//             System.out.println("given is a armstrong number");
+//         }
+//         else {
+//             System.out.println("given is not a armstrong number");
+//         }
+//     }
+// }
+
+
+// public class jul25{
+//     public static void main(String[] args) {
+//         System.out.println("rahul");
+//     }
+// }
+
+
+// import java.util.Scanner;
+// public class  jul25{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int c = a + b ;
+//         System.out.println("the addition of " +a+ " and " +b+ " is " +c);
+//     }
+// }
+
+
+
+
